69b2e2ae0d0e54488263743e04c09518a8def4a9
Peter Georgeson <peter@supernifty.org>
2017-10-12 15:18:25 +1100

    include and exclude files

--- find_size/find_size.py
+++ find_size/find_size.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 
+import argparse
 import os
 import collections
+import logging
+import re
 import sys
 
 def human(s):
@@ -16,20 +19,52 @@ def human(s):
     else:
         return '{:.1f}TB'.format(s / 1e12)
 
-extensions = collections.defaultdict(int)
-total = 0
+def find(dir, include, exclude):
+    extensions = collections.defaultdict(int)
+    extensions_count = collections.defaultdict(int)
+    total = 0
+    count = 0
+    skipped = 0
 
-for path, dirs, files in os.walk('.'):
-   for filename in files:
-       # add file size to the extension dictionary item, instead of add 1
-       try:
-           file_size = os.path.getsize(os.path.join(path,filename))
-           extensions[os.path.splitext(filename)[-1].lower()] += file_size
-           total += file_size
-       except OSError as e:
-           sys.stderr.write("ERROR: {}\n".format(e))
+    for path, dirs, files in os.walk(dir):
+       for filename in files:
+           # add file size to the extension dictionary item, instead of add 1 
+           try:
+               if args.include is not None and re.search(args.include, filename) is None:
+                   skipped += 1
+                   continue
+               if args.exclude is not None and re.search(args.exclude, filename) is not None:
+                   skipped += 1
+                   continue
 
-for key, value in sorted(extensions.items(), key=lambda item: (item[1], item[0])):
-    sys.stdout.write('%s,%s\n' % (key, human(value)))
+               file_size = os.path.getsize(os.path.join(path,filename))
+               extensions[os.path.splitext(filename)[-1].lower()] += file_size
+               extensions_count[os.path.splitext(filename)[-1].lower()] += 1
+               total += file_size
+               count += 1
+               if count % 1000 == 0:
+                   logging.debug('%i files processed. Total size %s. Skipped %i', count, human(total), skipped)
 
-sys.stdout.write('TOTAL,%s\n' % (human(total)))
+           except OSError as e:
+               logging.warn("%s", e)
+
+    sys.stdout.write('Extension,Size,Count\n')
+    for key, value in sorted(extensions.items(), key=lambda item: (item[1], item[0])):
+        sys.stdout.write('%s,%s,%i\n' % (key, human(value), extensions_count[key]))
+
+    sys.stdout.write('TOTAL,%s,%i\n' % (human(total), count))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='find total file sizes by extension')
+    parser.add_argument('--dir', default='.', help='')
+    parser.add_argument('--include', required=False, help='only include files matching pattern')
+    parser.add_argument('--exclude', required=False, help='exclude files matching pattern')
+    parser.add_argument('--verbose', action='store_true', default=False, help='more logging')
+    args = parser.parse_args()
+
+    if args.verbose:
+        logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', level=logging.DEBUG)
+    else:
+        logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', level=logging.INFO)
+
+    find(args.dir, args.include, args.exclude)
